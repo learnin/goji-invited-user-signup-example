7a2620ff6aba38c34cfd5e8124e765fe52371054
learnin <i.learnin@gmail.com>
2014-12-29 02:16:44 +0900

    Implement ldap search and add

--- server.go
+++ server.go
@@ -6,30 +6,37 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/flosch/pongo2"
+	"github.com/mavricknz/ldap"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
 
-type Page struct {
-	HashKey string
-	Msg     string
-	UserId  string
+type Form struct {
+	HashKey         string
+	Msg             string
+	UserId          string
+	Password        string
+	ConfirmPassword string
+	LastName        string
+	FirstName       string
+	Mail            string
 }
 
-var indexTpl = pongo2.Must(pongo2.FromFile("views/index.tpl"))
+var signupTpl = pongo2.Must(pongo2.FromFile("views/signup.tpl"))
 var completeTpl = pongo2.Must(pongo2.FromFile("views/complete.tpl"))
 
-func showIndexPage(c web.C, w http.ResponseWriter, r *http.Request) {
+func showSignupPage(c web.C, w http.ResponseWriter, r *http.Request) {
 	hashKey := c.URLParams["hashKey"]
 	if hashKey == "" {
 		http.Error(w, "", http.StatusNotFound)
 		return
 	}
-	var page Page
-	page.HashKey = hashKey
-	renderIndex(c, w, r, page)
+	var form Form
+	form.HashKey = hashKey
+	renderSignupPage(c, w, r, form)
 }
 
 func showCompletePage(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -40,31 +47,155 @@ func showCompletePage(c web.C, w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func renderIndex(c web.C, w http.ResponseWriter, r *http.Request, page Page) {
-	err := indexTpl.ExecuteWriter(pongo2.Context{"page": page}, w)
+func renderSignupPage(c web.C, w http.ResponseWriter, r *http.Request, form Form) {
+	err := signupTpl.ExecuteWriter(pongo2.Context{"form": form}, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-func authentication(c web.C, w http.ResponseWriter, r *http.Request) {
-	var page Page
-	page.UserId = r.FormValue("userId")
-	page.HashKey = r.FormValue("hashKey")
-	if page.UserId == "" {
-		page.Msg = "ユーザーIDを入力してください。"
-		renderIndex(c, w, r, page)
+func execute(c web.C, w http.ResponseWriter, r *http.Request) {
+	var form Form
+	form.UserId = r.FormValue("userId")
+	form.Password = r.FormValue("password")
+	form.ConfirmPassword = r.FormValue("confirmPassword")
+	form.LastName = r.FormValue("lastName")
+	form.FirstName = r.FormValue("firstName")
+	form.Mail = r.FormValue("mail")
+	form.HashKey = r.FormValue("hashKey")
+	if form.HashKey == "" {
+		http.Error(w, "", http.StatusNotFound)
 		return
 	}
-	fmt.Println(hash(page.UserId))
-	if hash(page.UserId) != page.HashKey {
-		page.Msg = "ユーザーIDを正しく入力してください。"
-		renderIndex(c, w, r, page)
+	if !validate(&form) {
+		form.Msg = strings.Replace(form.Msg, "\n", "<br/>", -1)
+		renderSignupPage(c, w, r, form)
+		return
+	}
+	fmt.Println(hash(form.UserId))
+	if hash(form.UserId) != form.HashKey {
+		form.Msg = "ユーザーIDを正しく入力してください。"
+		renderSignupPage(c, w, r, form)
+		return
+	}
+	alreadyExist, err := existsUser()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if alreadyExist {
+		form.Msg = "そのユーザーはすでに登録されています。"
+		renderSignupPage(c, w, r, form)
 		return
 	}
 	http.Redirect(w, r, "/complete", http.StatusFound)
+}
 
+func addUser(l *ldap.LDAPConnection) error {
+	dn := "cn=test3,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org"
+	var addAttrs []ldap.EntryAttribute = []ldap.EntryAttribute{
+		ldap.EntryAttribute{
+			Name: "objectclass",
+			Values: []string{
+				"person", "inetOrgPerson", "organizationalPerson", "top",
+			},
+		},
+		ldap.EntryAttribute{
+			Name: "uid",
+			Values: []string{
+				"test3",
+			},
+		},
+		ldap.EntryAttribute{
+			Name: "cn",
+			Values: []string{
+				"test3",
+			},
+		},
+		ldap.EntryAttribute{
+			Name: "givenName",
+			Values: []string{
+				"test3gn",
+			},
+		},
+		ldap.EntryAttribute{
+			Name: "sn",
+			Values: []string{
+				"test3sn",
+			},
+		},
+	}
+	addReq := ldap.NewAddRequest(dn)
+	for _, attr := range addAttrs {
+		addReq.AddAttribute(&attr)
+	}
+	fmt.Print(addReq)
+	err := l.Add(addReq)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func existsUser() (bool, error) {
+	ldapServer := "ipa.demo1.freeipa.org"
+	l := ldap.NewLDAPConnection(ldapServer, 389)
+	err := l.Connect()
+	if err != nil {
+		return false, err
+	}
+	defer l.Close()
+	err = l.Bind("uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org", "Secret123")
+	if err != nil {
+		return false, err
+	}
+	baseDN := "cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org"
+	var filter []string = []string{"(uid=test3)"}
+	var attributes []string = []string{
+		"uid", "givenname"}
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree, ldap.DerefAlways, 0, 0, false,
+		filter[0],
+		attributes,
+		nil)
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		return false, err
+	}
+	if len(sr.Entries) == 0 {
+		return false, addUser(l)
+	}
+	return true, nil
+}
+
+func validate(form *Form) bool {
+	if form.UserId == "" {
+		form.Msg += "ユーザーIDを入力してください。\n"
+	}
+	if form.Password == "" {
+		form.Msg += "パスワードを入力してください。\n"
+	}
+	if form.ConfirmPassword == "" {
+		form.Msg += "パスワード(確認)を入力してください。\n"
+	}
+	if form.LastName == "" {
+		form.Msg += "姓を入力してください。\n"
+	}
+	if form.FirstName == "" {
+		form.Msg += "名を入力してください。\n"
+	}
+	if form.Mail == "" {
+		form.Msg += "メールアドレスを入力してください。\n"
+	}
+	if form.Password != form.ConfirmPassword {
+		form.Msg += "パスワードとパスワード(確認)が一致していません。\n"
+	}
+	if form.Msg != "" {
+		return false
+	}
+	return true
 }
 
 func hash(s string) string {
@@ -75,8 +206,8 @@ func hash(s string) string {
 }
 
 func main() {
-	goji.Get("/:hashKey", showIndexPage)
-	goji.Post("/authentication", authentication)
-	goji.Get("/complete", showCompletePage)
+	goji.Get("/signup/:hashKey", showSignupPage)
+	goji.Post("/signup/execute", execute)
+	goji.Get("/signup/complete", showCompletePage)
 	goji.Serve()
 }
--- views/signup.tpl
+++ views/signup.tpl
@@ -5,15 +5,15 @@
     <title>ユーザー登録</title>
   </head>
   <body>
-    {{ page.Msg }}<br/>
-    <form action="authentication" method="post">
+    {{ form.Msg|safe }}<br/>
+    <form action="execute" method="post">
       ユーザーID:<input type="text" id="userId" name="userId"><br/>
-      新パスワード:<input type="password" id="password" name="password"><br/>
-      新パスワード(確認):<input type="password" id="confirmPassword" name="confirmPassword"><br/>
+      パスワード:<input type="password" id="password" name="password"><br/>
+      パスワード(確認):<input type="password" id="confirmPassword" name="confirmPassword"><br/>
       氏名:姓<input type="text" id="lastName" name="lastName"><br/>
       名<input type="text" id="firstName" name="firstName"><br/>
       メールアドレス:<input type="text" id="mail" name="mail"><br/>
-      <input type="hidden" id="hashKey" name="hashKey" value="{{ page.HashKey }}">
+      <input type="hidden" id="hashKey" name="hashKey" value="{{ form.HashKey }}">
       <input type="submit" value="次へ">
     </form>
   </body>
