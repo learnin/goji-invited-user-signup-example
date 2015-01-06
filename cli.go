14b5e392533dc4bf4530cd4383b6472ea5f40eb4
learnin <i.learnin@gmail.com>
2015-01-07 02:16:09 +0900

    Add feature cli for send invite mail

---
+++ cli.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/codegangsta/cli"
+
+	"github.com/learnin/goji-invited-user-signup-example/helpers"
+	"github.com/learnin/goji-invited-user-signup-example/models"
+)
+
+const SALT = "HsE@U91Ie!8ye8ay^e87wya7Y*R%38[0(*T[9w4eut[9e"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Usage = "fight the loneliness!"
+	app.Action = func(c *cli.Context) {
+		if err := action(c); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	app.Run(os.Args)
+}
+
+func action(c *cli.Context) error {
+	var ds helpers.DataSource
+	if err := ds.Connect(); err != nil {
+		return err
+	}
+	defer ds.Close()
+
+	var inviteUsers []models.InviteUser
+	if d := ds.GetDB().Where(&models.InviteUser{Status: models.STATUS_NOT_INVITED}).Find(&inviteUsers); d.Error != nil {
+		return d.Error
+	}
+	inviteUsersCount := len(inviteUsers)
+	if inviteUsersCount == 0 {
+		fmt.Println("未招待のユーザはありません。")
+		return nil
+	}
+	for i := 0; i < inviteUsersCount; i++ {
+		if err := ds.DoInTransaction(func(ds *helpers.DataSource) error {
+			inviteUser := inviteUsers[i]
+			inviteUser.InviteCode = helpers.Hash(strconv.FormatInt(inviteUser.Id, 10), SALT)
+			inviteUser.Status = models.STATUS_INVITED
+			now := time.Now()
+			inviteUser.InvitedAt = now
+			inviteUser.UpdatedAt = now
+			tx := ds.GetTx()
+			if err := tx.Save(inviteUser).Error; err != nil {
+				return err
+			}
+			return sendMail(inviteUser)
+		}); err != nil {
+			// FIXME エラーが発生してもスキップするようにする
+			return err
+		}
+	}
+	return nil
+}
+
+// FIXME 接続は1回にして、Reset, Mail, Rcpt, Data, w.Closeを宛先文回す。
+func sendMail(inviteUser models.InviteUser) error {
+	c, err := smtp.Dial("localhost:1025")
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	// auth := smtp.PlainAuth("", "user", "pass", "localhost")
+
+	if err := c.Hello("localhost"); err != nil {
+		return err
+	}
+	// if ok, _ := c.Extension("AUTH"); ok {
+	// 	if err := c.Auth(auth); err != nil {
+	// 		return err
+	// 	}
+	// }
+
+	c.Mail("sender@example.org")
+	if err := c.Rcpt(inviteUser.Mail); err != nil {
+		return err
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	// FIXME エンコード
+	msg := "From: sender@example.org\r\n" +
+		"To: " + inviteUser.Mail + "\r\n" +
+		"Subject: test\r\n" +
+		"\r\n" +
+		"テストメールです"
+	if _, err = w.Write([]byte(msg)); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	return c.Quit()
+}
