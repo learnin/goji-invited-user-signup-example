3e2f3122b11942cb47aa0c81614a7a48512da7cf
learnin <i.learnin@gmail.com>
2015-01-12 02:17:31 +0900

    Implement mail subject and body encode by iso-2022-jp and base64

--- helpers/smtp.go
+++ helpers/smtp.go
@@ -1,9 +1,17 @@
 package helpers
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/smtp"
+	"strings"
+	"time"
+	"unicode/utf8"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 const SMTP_CONFIG_FILE = "config/smtp.json"
@@ -59,18 +67,65 @@ func (util *SmtpUtil) SendMail(c *smtp.Client, to string) error {
 	if err != nil {
 		return err
 	}
-	// FIXME エンコード
-	msg := "Content-Type: text/plain; charset=ISO-2022-JP\r\n" +
-		"Content-Transfer-Encoding: 7bit\r\n" +
-		"From: " + smtpCfg.From + "\r\n" +
+	subject, err := encodeSubject("テストおおおおおお。あああいいいいんんんaいいう1234あああああああああああいいいいいいいいいいう")
+	if err != nil {
+		return err
+	}
+	body, err := encodeToJIS("テストメールです" + "\r\n")
+	if err != nil {
+		return err
+	}
+	msg := "From: " + smtpCfg.From + "\r\n" +
 		"To: " + to + "\r\n" +
-		"Subject: test\r\n" +
+		"Subject:" + subject +
+		"Date: " + time.Now().Format(time.RFC1123Z) + "\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"Content-Type: text/plain; charset=ISO-2022-JP\r\n" +
+		"Content-Transfer-Encoding: 7bit\r\n" +
 		"\r\n" +
-		"テストメールです"
+		body
 	if _, err = w.Write([]byte(msg)); err != nil {
 		return err
 	}
 	return w.Close()
+}
+
+func encodeToJIS(s string) (string, error) {
+	r, err := ioutil.ReadAll(transform.NewReader(strings.NewReader(s), japanese.ISO2022JP.NewEncoder()))
+	if err != nil {
+		return "", err
+	}
+	return string(r), nil
+}
+
+func encodeSubject(subject string) (string, error) {
+	b := make([]byte, 0, utf8.RuneCountInString(subject))
+	for _, s := range splitByCharLength(subject, 13) {
+		b = append(b, " =?ISO-2022-JP?B?"...)
+		s, err := encodeToJIS(s)
+		if err != nil {
+			return "", err
+		}
+		b = append(b, base64.StdEncoding.EncodeToString([]byte(s))...)
+		b = append(b, "?=\r\n"...)
+	}
+	return string(b), nil
+}
+
+func splitByCharLength(s string, length int) []string {
+	result := []string{}
+	b := make([]byte, 0, length)
+	for i, c := range strings.Split(s, "") {
+		b = append(b, c...)
+		if i%length == 0 {
+			result = append(result, string(b))
+			b = make([]byte, 0, length)
+		}
+	}
+	if len(b) > 0 {
+		result = append(result, string(b))
+	}
+	return result
 }
 
 // FIXME 接続は1回にして、Reset, Mail, Rcpt, Data, w.Closeを宛先文回す。
